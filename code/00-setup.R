4389364769898df55735b204f5bbdc1d61b096e5
Sharla Gelfand <sharla.gelfand@gmail.com>
2023-09-14 10:10:05 -0400

    Add code to download workshop

--- code/00-setup.R
+++ code/00-setup.R
@@ -21,4 +21,4 @@ install.packages(setdiff(packages, rownames(installed.packages())))
 
 library(usethis)
 
-use_course("posit-conf-2023/creative-coding")
+use_course("sharlagelfand/creative-coding-workshop")
